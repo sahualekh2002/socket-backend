be73fb3e6559a65bb033d34fd39ab3d72abbeff1
sahualekh2002 <sahualekh2002@gmail.com>
2026-02-11 15:21:45 +0530

    Updated backend structure with contact form and socket.io

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,12 +1,18 @@
+
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,          // ✅ use 587 (NOT 465)
+  secure: false,      // ✅ false for 587
+  // family: 4,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Gmail App Password
+    pass: process.env.EMAIL_PASS,
   },
 });
+
 
 module.exports = async function sendMail({ name, email, message, attachments = [] }) {
   await transporter.sendMail({
